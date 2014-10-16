ae6f199899f3cb206bdcf3d6b66c2dc264bfd8c2
fsuarez6 <fsuarez6@gmail.com>
2014-10-16 23:37:22 +0200

    Fixed max iterations step script for the iterative decoupling

--- iterative_decoupling/max_iter_step_size.m
+++ iterative_decoupling/max_iter_step_size.m
@@ -6,7 +6,7 @@ cd('../');
 startup;
 cd(script_path);
 
-clear; close all;
+clear; close all; clc;
 home_path = getuserdir();
 data_folder = [getuserdir() '/journal_data/'];
 % Get the files that include the pattern
@@ -28,30 +28,41 @@ end
 [step_size, idx] = sort(max_step_size);
 x = 1:length(idx);
 y1 = solved(idx);
-%~ y2 = mean_time(idx) .^ (-1);
-y2 = mean_time(idx);
+y2 = mean_time(idx) .^ (-1);
+%~ y2 = mean_time(idx) * 1000;
 set(0,'defaulttextinterpreter','latex');
 figure,
 [ax,h1,h2] = plotyy(x, y1, x, y2);
 % Fix the figure axis
+% X axis
 xlim(ax(1), [1 length(idx)]);
 xlim(ax(2), [1 length(idx)]);
-set(ax(2),'xticklab',[],'xtick',[]);
 set(ax(1),'XTick', x);
 set(ax(1),'XTickLabel', step_size);
+set(ax(2),'xticklab',[],'xtick',[]);
+% Y axis
+y1_lim = [0 100];
+y2_lim = [0 800];
+yticks = 10;
+ylim(ax(1), y1_lim);
+ylim(ax(2), y2_lim);
+set(ax(1),'YTick', y1_lim(1):y1_lim(2)/yticks:y1_lim(2));
+set(ax(2),'YTick', y2_lim(1):y2_lim(2)/yticks:y2_lim(2));
+% Legend
 set(h2,'LineStyle','--');
-set(h1,'LineWidth','0.8');
-set(h2,'LineWidth','0.8');
-lh = legend('Solved', 'Mean Iteration Time');
-set(lh,'Location','SouthEast');
+set(h1,'LineWidth', 0.8);
+set(h2,'LineWidth', 0.8);
+lh = legend('Solved', 'Mean Solver Rate');
+set(lh,'Location','East');
 rotateXLabels(ax(1), 60);
 grid on;
 axes(ax(1)); ylabel('Solved [\%]');
-axes(ax(2)); ylabel('Mean Iteration Time [ms]');
+axes(ax(2)); ylabel('Mean Solver Rate [Hz]');
 xlabel('Maximum Increment per Iteration [rad]');
 
 % Generate the tikz file
+extra_opts = {'tick label style={font=\small}', 'label style={font=\small}', 'legend style={font=\small}'};
 tikz_folder = [getuserdir() '/git/phd-thesis/tikz/'];
 
 matlab2tikz([tikz_folder 'deco_step_size.tex'], 'standalone', true, 'showInfo',false,'height',...
-        '50mm','width','150mm','floatFormat','%.4f');
+        '50mm','width','120mm','floatFormat','%.4f');
