cba6f1014a9b9b6a026215dddfbde4bef05e0c82
fsuarez6 <fsuarez6@gmail.com>
2014-10-20 20:20:59 +0200

    happy monday _ bleh _

--- iterative_decoupling/method_iterations.m
+++ iterative_decoupling/method_iterations.m
@@ -6,16 +6,17 @@ cd('../');
 startup;
 cd(script_path);
 
-clear; close all;
+clc; clear; close all;
 home_path = getuserdir();
 data_folder = [getuserdir() '/journal_data/'];
 methods = {'kdl','lma','decoup'};
-labels = {'N--R','L--M','I--D'};
+labels = {'Iterative Decoupling', 'Levenberg--Marquardt', 'Newton--Raphson'};
 % Get the files that include the pattern
 pattern = '_iter_';
 extension = '.mat';
 filenames = get_files_by_pattern(data_folder, ['*' extension], pattern);
-count = ones(length(methods), 1);
+solved_count = ones(length(methods), 1);
+loop_count = ones(length(methods), 1);
 
 for f = 1:length(filenames)
   for m = 1:length(methods)
@@ -27,15 +28,26 @@ for f = 1:length(filenames)
     load([data_folder filename]);
     idx = iterations > 0;
     n = sscanf(filename, [method pattern '%d' extension]);
+    results(m).iterations(loop_count(m)) = n(1);
+    loop_count(m) = loop_count(m) + 1;
     mean_time = mean(time(idx));
     if isnan(mean_time)
       continue;
     end
-    results(m).iter_number(count(m)) = n(1);
-    results(m).mean_time(count(m)) = mean_time;
-    results(m).median_time(count(m)) = median(time(idx));
-    results(m).solved(count(m)) = 100 * sum(idx) / length(iterations);
-    count(m) = count(m) + 1;
+    results(m).iter_number(solved_count(m)) = n(1);
+    results(m).mean_time(solved_count(m)) = mean_time;
+    results(m).median_time(solved_count(m)) = median(time(idx));
+    results(m).solved(solved_count(m)) = 100 * sum(idx) / length(iterations);
+    solved_count(m) = solved_count(m) + 1;
+  end
+end
+sorted_iterations = sort(results(1).iterations);
+
+for m = 1:length(methods)
+  [method_iter, sorted_idx] = sort(results(m).iter_number);
+  plots_data(m).y = results(m).solved(sorted_idx);
+  for i = 1:length(results(m).iter_number)
+    plots_data(m).x(i) = find(method_iter(i)==sorted_iterations);
   end
 end
 
@@ -43,25 +55,25 @@ ColorSet = distinguishable_colors(3);
 set(0,'DefaultAxesColorOrder', ColorSet);
 set(0,'defaulttextinterpreter','latex');
 
-% KDL
-[kdl_iter, idx] = sort(results(1).iter_number);
-kdl_solved = results(1).solved(idx);
-disp(idx)
-% LMA
-[lma_iter, idx] = sort(results(2).iter_number);
-lma_solved = results(2).solved(idx);
-disp(idx)
-% Decoupling
-[deco_iter, idx] = sort(results(3).iter_number);
-deco_solved = results(3).solved(idx);
-disp(idx)
-
 % Plot
-figure; hold on; grid on;
-plot(deco_iter, deco_solved, ':rs', 'LineWidth', 0.8);
-plot(kdl_iter, kdl_solved, '-gd', 'LineWidth', 0.8);
-plot(lma_iter, lma_solved, '--bo', 'LineWidth', 0.8);
+figure; hold on; grid on; box on;
+plot(plots_data(3).x, plots_data(3).y, '-rs', 'LineWidth', 0.8);
+plot(plots_data(2).x, plots_data(2).y, '--gd', 'LineWidth', 0.8);
+plot(plots_data(1).x, plots_data(1).y, ':bo', 'LineWidth', 0.8);
+lh = legend(labels);
+set(lh, 'Location', 'NorthWest');
+xlim([0 25]);
+set(gca,'XTick', 1:24);
+set(gca,'XTickLabel', sorted_iterations);
+xh = get(gca,'XLabel');
+set(xh,'Position',get(xh,'Position') - [0 .2 0]);
+rotateXLabels(gca, 60);
+set(gca,'YTick', 0:10:100);
+xlabel('Number of Iterations');
+ylabel('Solved [\%]');
+% Generate the tikz file
+extra_opts = {'tick label style={font=\footnotesize}', 'label style={font=\small}', 'legend style={font=\footnotesize}'};
+tikz_folder = [getuserdir() '/git/phd-thesis/tikz/'];
 
-%~ set(gca,'XTick', );
-%~ set(gca,'XTickLabel', step_size);
-tilefigs;
+matlab2tikz([tikz_folder 'deco_iterations.tex'], 'standalone', true, 'showInfo',false,'height',...
+        '50mm','width','120mm','floatFormat','%.4f','extraAxisOptions',extra_opts);
--- iterative_decoupling/spent_time.m
+++ iterative_decoupling/spent_time.m
@@ -7,15 +7,17 @@ startup;
 cd(script_path);
 
 % Performance comparison
-clear; close all;
+clear; close all; clc;
 home_path = getuserdir();
 %~ data_folder = [getuserdir() '/Dropbox/doctorado/publications/2014_kinematics/test/'];
 data_folder = [getuserdir() '/journal_data/'];
-methods = {'kdl','lma','ikfast','decoup'};
-labels = {'N--R','L--M','IKFast','I--D'};
+methods = {'kdl','lma','decoup','ikfast'};
+labels = {'N--R','L--M','I--D','IKFast'};
 [rows, cols] = size(methods);
 % boxplot data
 disp('Method  median  upper_wisker  lower_wisker   upper_adjacent_value   lower_adjacent_value');
+anova_data = [];
+anova_groups = [];
 for i = 1:cols
   name = methods{i};
   load([data_folder name '.mat']);
@@ -34,6 +36,8 @@ for i = 1:cols
   lav = stats.lower_adjacent_value;
   uav = stats.upper_adjacent_value;
   disp([labels{i} '   ' num2str([m uw lw uav lav])]);
+  anova_data = [anova_data;results(i).time * 1000];
+  anova_groups = [anova_groups;i*ones(length(results(i).time),1)];
 end
 
 % Performance table
@@ -43,40 +47,42 @@ for i = 1:cols
   disp([labels{i} '   ' num2str([results(i).solved results(i).stats.median results(i).stats.mean])]);
 end
 
-% Time histograms
-tikz_folder = [getuserdir() '/git/kinematics-journal/tikz/'];
-set(0,'defaulttextinterpreter','latex');
-titles = {'Newton-Raphson','Levenberg-Marquardt','IKFast','Iterative-Decoupling'};
-filenames = {'kdl_time_hist','lma_time_hist','ikfast_hist','decoup_time_hist'};
-for i = 1:cols
-  figure,
-  xlimit = 25;
-  num_bars = 100;
-  hist(results(i).time * 1000, 0:xlimit / num_bars:xlimit);
-  %hist(results(i).time * 1000, num_bars);
-  xlabel('Time [ms.]');
-  ylabel('log(Frequency)');
-  title(titles{i});
-  % Workaround
-  % Get histogram patches
-  ph = get(gca,'children');
-  % Determine number of histogram patches
-  N_patches = length(ph);
-  for j = 1:N_patches
-   % Get patch vertices
-   vn = get(ph(j),'Vertices');
-   % Adjust y location
-   vn(:,2) = vn(:,2) + 1;
-   % Reset data
-   set(ph(j),'Vertices',vn)
-  end
-  % Change scale
-  set(gca,'yscale','log')
-  % Save the tikz figures
-  grid on;
-  xlim([0 xlimit]);
-  % ylim([0 1e5]);
+anova1(anova_data, anova_groups);
+
+%~ % Time histograms
+%~ tikz_folder = [getuserdir() '/git/kinematics-journal/tikz/'];
+%~ set(0,'defaulttextinterpreter','latex');
+%~ titles = {'Newton-Raphson','Levenberg-Marquardt','IKFast','Iterative-Decoupling'};
+%~ filenames = {'kdl_time_hist','lma_time_hist','ikfast_hist','decoup_time_hist'};
+%~ for i = 1:cols
+  %~ figure,
+  %~ xlimit = 25;
+  %~ num_bars = 100;
+  %~ hist(results(i).time * 1000, 0:xlimit / num_bars:xlimit);
+  %~ %hist(results(i).time * 1000, num_bars);
+  %~ xlabel('Time [ms.]');
+  %~ ylabel('log(Frequency)');
+  %~ title(titles{i});
+  %~ % Workaround
+  %~ % Get histogram patches
+  %~ ph = get(gca,'children');
+  %~ % Determine number of histogram patches
+  %~ N_patches = length(ph);
+  %~ for j = 1:N_patches
+   %~ % Get patch vertices
+   %~ vn = get(ph(j),'Vertices');
+   %~ % Adjust y location
+   %~ vn(:,2) = vn(:,2) + 1;
+   %~ % Reset data
+   %~ set(ph(j),'Vertices',vn)
+  %~ end
+  %~ % Change scale
+  %~ set(gca,'yscale','log')
+  %~ % Save the tikz figures
+  %~ grid on;
+  %~ xlim([0 xlimit]);
+  %~ % ylim([0 1e5]);
   %~ matlab2tikz([tikz_folder filenames{i} '.tex'], 'standalone', true, 'showInfo',false,'height',...
         %~ '50mm','width','50mm','floatFormat','%.4f');
-end
+%~ end
 tilefigs;
